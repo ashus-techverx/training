9e1ac167a34ef8e42fbcd86ddb97bdf536c375f8
ashus-techverx <ashus@gems.techverx.com>
2016-01-08 19:24:47 +0500

    second test commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'calender/show'
+
   get 'thecustomroute' => 'user#show'
   get 'user/show2'
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
